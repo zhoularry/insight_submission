0882edf15c48425323a65cf15bdc8ffe8eb57a43
zhoularry <zhou.larry@gmail.com>
2015-03-20 19:08:30 -0400

    add correct pseudo code for the median challenge.

--- median_challenge.py
+++ median_challenge.py
@@ -1,14 +1,25 @@
 #2 median words in a line
 	
-# order the files in alphabetical order and 
-# add text files to the bottom of the ones prior to it
-
+# order the files in alphabetical order
 # read in file line by line
-# deliniate by 1 space and count how many words on in it
+# deliniate by 1 space and count how many words on in each line
+# add that number of words in the dictionary to the dictionary.
+# if the number of words in the line has been seen before, 
+# #   then we add 1 to the key in the dictionary
+# if the number of words in the line has not been seen before,
+#   then we add that number as a key in the dictionary
+# +1 to a counter that finds out how many lines have been read
+# if the counter = even, then we go through the keys to whatever 
+# key that gives us where n/2 falls
+# if the counter = odd, then we go through to the n/2 where that falls
+# and take the key of the dic --- make it a float 1.0
+# move onto next line
+# repeat
+ 
+# if we can use a median() function, then 
+# count the words in the line and dump that into an array.
+# then just take the median() function of the array. spit that out before going onto the next line
 
-# for loop through the 
-
-import csv
 import glob
 import json
 import pdb # debugger. remove
@@ -16,32 +27,30 @@ import pdb # debugger. remove
 file_list = sorted(glob.glob('wc_input/*.txt'))
 # pdb.set_trace()
 
-	# file = open('wc_input/wc_input.txt', 'r')
-
 # create a dictionary
-worddic = {} 
+line_count = {} 
+# wordcount for the line
+wordcount = 0
 
-# read the file into the dictionary
+# read the file into the dictionary in alphabetic order
 # puts everything into lowercase
 
 for input_file in file_list:
 	with open(input_file, 'r') as curfile:
 
 		for word in curfile.read().split():
-
-				# word does not exist in the dictionary. starts with 1
-				clean_word = ''.join(e for e in word if e.isalnum())
+			wordcount += 1
 				
-				if clean_word not in worddic:
-					worddic[clean_word.lower().strip()] = 1
+				if clean_word not in line_count:
+					line_count[clean_word.lower().strip()] = 1
 				# word does exist in the dictionary. +1
 				else:
-					worddic[clean_word.lower().strip()] += 1
+					line_count[clean_word.lower().strip()] += 1
 
-for a,b in worddic.items():
+for a,b in line_count.items():
 	print a,b
 
 with open('wc_output/med_result.txt', 'w') as f:
-	data = [worddic]
+	data = [line_count]
 	json.dump(data, f)
 
