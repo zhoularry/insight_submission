5fea6d22abbcc9992c1467a70597c8e5ce92f221
zhoularry <zhou.larry@gmail.com>
2015-03-20 00:47:12 -0400

    fixed wc_result.txt to include the value of the dictionary key and also be on separate lines!

--- wordcount_challenge.py
+++ wordcount_challenge.py
@@ -11,47 +11,54 @@
 # sort the dictionary by word
 # display dic along with counts
 
+# write the sorted dic with all counts into the .txt final
+###################
+
 import csv
 import glob
 import json
 import pdb # debugger. remove
 
+# sorts the files in alpha order and takes the 1 at a time
 file_list = sorted(glob.glob('wc_input/*.txt'))
 # pdb.set_trace()
-
-	# file = open('wc_input/wc_input.txt', 'r')
 
 # create a dictionary
 worddic = {} 
 
-# read the file into the dictionary
-# puts everything into lowercase
-
+# read the current file into the dictionary
 for input_file in file_list:
 	with open(input_file, 'r') as curfile:
 
+		# going line by line (read), word for word (split)
 		for word in curfile.read().split():
 
-				# remove the punctuation
+				# remove the punctuation from the word
 				clean_word = ''.join(e for e in word if e.isalnum())
 
-				# word does not exist in the dictionary. starts with 1
+				# word does not exist in the dictionary. start with 1
 				# remove any spaces and makes all lowercase
 				if clean_word not in worddic:
 					worddic[clean_word.lower().strip()] = 1
-				# word does exist in the dictionary. +1
+				# if word does exist in the dictionary, +1
 				else:
 					worddic[clean_word.lower().strip()] += 1
 
-sorteddic = {}
+# sort dictionary by key
+s = sorted (worddic.keys())
+	
+# show sorted dic
+for key in s:
+ 	print key, worddic[key]
 
 # sort dictionary by key
-for key in sorted(worddic.iterkeys()):	
-	print "%s: %s" % (key, worddic[key])
+# for key in sorted(worddic.iterkeys()):	
+#  	print "%s: %s" % (key, worddic[key])
 
 with open('wc_output/wc_result.txt', 'w') as f:
-	data = [worddic]
-	json.dump(data, f)
+
+	for key in sorted(worddic.keys()):
+	 	f.write(key + " " + str(worddic[key]) +'\n')
 
 # NLTK part
 
