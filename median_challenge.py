7a80668ea4efe1a4855d894823d6fa502d7409b3
zhoularry <zhou.larry@gmail.com>
2015-03-21 15:19:22 -0400

    add run.sh shell. cleaned up median output

--- median_challenge.py
+++ median_challenge.py
@@ -1,17 +1,19 @@
 #2 median words in a line
-	
+
+########## PSEUDO
+
 # order the files in alphabetical order
-# read in file line by line
-# deliniate by 1 space and count how many words on in each line
+# read in file line by line, count how many words on in each line
 # add that number of words in the dictionary to the dictionary.
 # if the number of words in the line has been seen before, 
-# #   then we add 1 to the key in the dictionary
+#    then we add 1 to the key in the dictionary
 # if the number of words in the line has not been seen before,
-#   then we add that number as a key in the dictionary
-# +1 to a counter that finds out how many lines have been read
-# if the counter = even, then we go through the keys to whatever 
-# key that gives us where n/2 falls
-# if the counter = odd, then we go through to the n/2 where that falls
+#    then we add that number as a key in the dictionary
+# +1 to a line counter
+# if the counter = even, 
+#	  then get middle avg between middle 2 keys
+# if the counter = odd, 
+#    then find middle x
 # and take the key of the dic --- make it a float 1.0
 # move onto next line
 # repeat
@@ -22,7 +24,6 @@
 
 import glob
 import json
-import pdb # debugger. remove
 
 file_list = sorted(glob.glob('wc_input/*.txt'))
 
@@ -39,7 +40,6 @@ for input_file in file_list:
 	with open(input_file, 'r') as curfile:
 
 		# wordcount for the line needs to refresh every time a new line hits
-
 		for line in curfile:
 
 			# counts words in a line		
@@ -58,8 +58,8 @@ for input_file in file_list:
 			if len(line_count) == 1:
 				for key, value in line_count.items():
 					print float(key)
+			
 			# all other cases
-
 			else:
 
 				# even -- take the sum(middle 2 keys) divide by 2
@@ -67,22 +67,16 @@ for input_file in file_list:
 					while True:
 						# sums up the key's definition and sums to strike
 						for a,b in line_count.items():
-							# pdb.set_trace()
 							
 							# add to the running total for the strike to jump out
 							strike += b
-							# print strike
-							# print counter
-							# print (counter)/2
 
 							# now we have the middle left key in a 
 							if strike > float(counter)/2:
 								strike = 0
 								while True:
 									for c,d in line_count.items():
-										# print strike
 										strike += d
-										# print strike
 										if strike >= float(counter)/2:
 											break
 									break
@@ -97,42 +91,14 @@ for input_file in file_list:
 					while True:
 						# sums up the key's definition and sums to strike
 						for a,b in line_count.items():
-							# pdb.set_trace()
 							
 							# add to the running total for the strike to jump out
 							strike += b 
 							if strike > counter/2:
 								break
-						# if strike < float(counter)/2:
 						median_words = float(a)
 						print median_words
 						break	
-				# # even
-				# if counter%2 == 0:
-				# 	# only go through if the strike is < n/2
-				# 	while strike < counter/2:
-				# 		# sums up the key's definition and sums to strike
-				# 		for a,b in line_count.items():
-				# 			strike += b
-				# 		median_words = float(a)
-				# 		print median_words
-
-				# # odd
-				# elif counter%2 == 1:
-
-				# 	while strike < counter/2:
-				# 		for a,b in line_count.items():
-				# 			strike += b
-				# 		median_words = float(a)
-				# 		print median_words
-
-print line_count.items()
-
-#prints the dictionary with #words | #lines
-for a,b in line_count.items():
-	print a,b
-	strike += b
-print strike
 
 with open('wc_output/med_result.txt', 'w') as f:
 	data = [line_count]
---
+++ run.sh
@@ -0,0 +1,6 @@
+# !/usr/bin/bash
+
+python wordcount_challenge.py
+python median_challenge.py
+
+chmod +x job.sh
